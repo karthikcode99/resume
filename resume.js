1947059cb761cb1e1e82e106fdd50a008d61d854
karthikcode99 <85825128+karthikcode99@users.noreply.github.com>
2021-08-02 12:36:54 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,22 @@
+// const resume=[
+//     {
+//         "name":"karthik.v",
+//         "Email":"karthikprime9626@gmail.com",
+//         "phone":"7708029601",
+//         "career":{
+//             "Objective":"Seeking a career to utilize my learnings, knowledge ,personal skills and training to gaincomprehensive understanding and significantly.",
+//         },
+//         "Field-of-intrest":"Software development,Volunteer work and Community Involvement.",
+//         "Achievements":"Won 2nd price  project exhibition contest at K.SRangasamy CollegeofTechnology.",
+//         "Technical-interest":{
+//             "languages":"C",
+//             "WebTechnologies":"HTML5, CSS, JAVASCRIPT, NODEjs, MONGODB, React js"
+//         },
+//         "Personal-details":{
+//             "FatherName":"Mr.N.vijayakumaran",
+//             "Date of Birth":"28.11.1999",
+//             "Languages Known":"English & Tamil",
+//             "Present Address":"16 northmain street melaulur thanjavur",
+//         }
+//     }
+// ]
